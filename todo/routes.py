0c36569789da1e912b0197df37157740009adc74
xkadzama <abc@gmail.com>
2026-05-26 16:54:56 +0300

    Реализовали add_task и delete_task

--- todo/routes.py
+++ todo/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 
 task_bp = Blueprint('tasks', __name__, template_folder='templates')
@@ -7,7 +7,8 @@ tasks_db = [
     {'id': 1, 'title': 'Купить хлеб', 'description': 'Успеть до закрытия'},
     {'id': 2, 'title': 'Заменить масло', 'description': 'Заехать в СТО'},
     {'id': 3, 'title': 'Выполнить ДЗ', 'description': 'До след. урок'},
-    {'id': 4, 'title': 'Реализовать CRUD', 'description': 'Дедлайн до завтра'}
+    {'id': 4, 'title': 'Реализовать CRUD', 'description': 'Дедлайн до завтра'},
+
 ]
 
 # CRUD - CREATE READ UPDATE DELETE
@@ -28,5 +29,19 @@ def detail_task(id):
     return render_template('detail.html', task_one=task_one)
 
 
+@task_bp.route('/add', methods=['GET', 'POST'])  # /tasks/add
+def add_task():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        tasks_db.append({'id': len(tasks_db)+1, 'title': title, 'description': description})
+        return redirect(url_for('tasks.get_all_tasks'))
+    return render_template('add_task.html')
 
+
+@task_bp.route('/delete/<int:id>', methods=['POST'])
+def delete_task(id):
+    print(id)
+    # Удалить задачу из БД по ID
+    return redirect(url_for('tasks.get_all_tasks'))
 
